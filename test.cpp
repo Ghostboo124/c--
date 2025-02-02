bc11163902e386afffd28d7598aeee2668101f76
Ghostboo-124 <nat.perkins11@gmail.com>
2025-02-02 14:42:15 +1000

    Adding support for Microsoft Store installed Spotify

--- test.cpp
+++ test.cpp
@@ -11,20 +11,23 @@ int main()
     std::cout << "Please enter the program that you want to open:\x20";
     getline(std::cin, app);
     std::cout << std::endl;
-    // std::cout << "Test!" << std::endl;
-    // bool isSpotify = std::regex_match(app, std::regex("((?i)spotify"));
-    // bool isVsCode = std::regex_match(app, std::regex("(?i)vscode"));
+    std::cout << "Test!" << std::endl;
+    bool isSpotify = std::regex_match(app, std::regex("spotify"));
+    // bool isVsCode = std::regex_match(app, std::regex("vscode"));
 
-    // if (isSpotify) {
-    if (app == "spotify") {
-        code = std::system("start C:\\Users\\perkinsal\\AppData\\Roaming\\Spotify\\Spotify.exe");
+    if (isSpotify) {
+    // if (app == "spotify") {
+		code = std::system("start Spotify");
+		if (code != 0) {
+			code = std::system("start C:\\Users\\perkinsal\\AppData\\Roaming\\Spotify\\Spotify.exe");
+		}
     }
     else if (app == "vscode") {
         code = std::system("start C:\\Users\\perkinsal\\AppData\\Local\\Programs\\\"Microsoft VS Code Insiders\"\\\"Code - Insiders.exe\"");
     }
     else if (app == "" || app == "\x20") {
-        std::cout << "Uhh, that isn't a valid input"
-    }
+        std::cout << "Uhh, that isn't a valid input";
+    } 
     else {
         std::cout << "The program " << app << " is unknown?";
         std::system("pause");
